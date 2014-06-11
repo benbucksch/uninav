f6240fe150e31204715e506d484ba7f4729d0784
Ben Bucksch <ben.bucksch@beonex.com>
2014-06-12 01:25:11 +0200

    Make a tile with an image on top

--- navview.js
+++ navview.js
@@ -1,10 +1,16 @@
+/**
+ * Copyright 2014 Ben Bucksch
+ * Published as AGPLv3
+ */
+
 var renderer;
 var camera;
 var scene;
 
 function onLoad() {
   createScene();
-  addTile(scene);
+  //addTile(null, "Earth", "img/car.jpg");
+  addTile(null, "Car", "img/car.jpg");
   render();
 }
 
@@ -13,7 +19,7 @@ window.addEventListener("load", onLoad, false);
 function createScene() {
   var parentE = document.getElementById("navview");
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75,
+  camera = new THREE.PerspectiveCamera(30,
     parentE.clientWidth / parentE.clientHeight, 0.1, 1000);
   camera.position.z = 5;
   if (window.WebGLRenderingContext) {
@@ -23,14 +29,34 @@ function createScene() {
   }
   renderer.setSize(parentE.clientWidth, parentE.clientHeight);
   parentE.appendChild(renderer.domElement);
+
+  renderer.setClearColor(0x000000, 1); // black
+
   return scene;
 }
 
-function addTile(parentN) {
-  var geometry = new THREE.CubeGeometry(1, 1, 1);
-  var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  var cube = new THREE.Mesh(geometry, material);
-  parentN.add(cube);
+/**
+ * @param parentTile {THREE.Mesh} another tile created by addTile.
+ *     Use |null| for root nodes.
+ * @param title {String} user-visible name of the tile
+ * @param imageURL {String} relative URL of the image, e.g.
+ *     "img/window.png" or "/img/window.jpg"
+ * @param clickCallback {Function} Called when the user
+ *     clicks on the node
+ * @returns {THREE.Mesh} The resulting tile node.
+ *     It's already added to the scene.
+ *     You need this as |parentTile| for child tiles.
+ */
+function addTile(parentTile, title, imageURL, clickCallback) {
+  var tile = new THREE.PlaneGeometry(1, 1);
+  var texture = new THREE.ImageUtils.loadTexture(imageURL);
+  var material = new THREE.MeshBasicMaterial({
+    map : texture,
+    side : THREE.FrontSide,
+  });
+  var node = new THREE.Mesh(tile, material);
+  node.rotation.x = -0.9;
+  scene.add(node);
 }
 
 function render() {
