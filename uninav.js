b13cab42c6ebd75c429bb90073a9fd5bdda3a457
Ben Bucksch <ben.bucksch@beonex.com>
2014-08-18 15:46:27 +0200

    Revert "Change background of Uninav when clicking topic"
    This reverts commit 3ba2ca213a1f9d76920060a816d042c674495062.

--- uninav.js
+++ uninav.js
@@ -239,7 +239,7 @@ function tiltTile(tile) {
 }
 
 /**
- * Action: Undo tiltTile()
+ * Action: Undo tileTile()
  * When: user clicked on another tile
  */
 function untiltTile(tile) {
@@ -256,14 +256,6 @@ function untiltTile(tile) {
             .to({ x : tile.label.oldRotation.x }, 250)
             .start();
   tile.label.rotation = null;
-}
-
-/**
- * Set the background image of the uninav.
- * When: the user clicked on a tile
- */
-function setBackground(imageURL) {
-  E("uninav").setAttribute("style", "background-image: url(" + imageURL + ")");
 }
 
 function render(time) {
@@ -301,9 +293,8 @@ function onMouseClick(event) {
 
     var oldN = selectedN;
     selectedN = tile;
-    //tiltTile(selectedN);
-    //untiltTile(oldN);
-    setBackground(imageRootURL + selectedN.node.img);
+    tiltTile(selectedN);
+    untiltTile(oldN);
   }
 }
 
