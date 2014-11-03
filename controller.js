d0f11b5dbe00c152dfe8e88b953e1f8406efdab9
Ben Bucksch <ben.bucksch@beonex.com>
2014-11-03 23:15:24 +0100

    factor out arrangeBelow()

--- controller.js
+++ controller.js
@@ -22,6 +22,7 @@ function onLoad() {
 
   loadRootTopic(function(rootTopic) {
     var rootN = new ThreeTile(rootTopic, null);
+    setRootObj(rootN);
     rootN.showChildren();
     var startN = rootN.children[0];
     assert(startN, "Start node not found. Taxonomy file broken?");
